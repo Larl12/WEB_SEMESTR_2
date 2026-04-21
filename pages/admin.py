605fabe2d221027c53302740838cc4c92dbb630e
Demin Ilya <semen.k.33@mail.ru>
2026-04-21 23:45:59 +0300

    feat: configure Docker PostgreSQL and Book model

--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from pages.models import Book
 
-# Register your models here.
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'copies_available', 'created_at')
+    list_filter = ('created_at',)
+    search_fields = ('title', 'description')
+    ordering = ('-created_at',)
+    readonly_fields = ('created_at',)
+    fields = ('title', 'description', 'copies_available', 'created_at')
